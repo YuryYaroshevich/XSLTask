2896f5c6ddbc84457c21ea005c822c95ca1909f3
Yury Yaroshevich <Yury_Yaroshevich@epam.com>
2013-06-04 11:20:40 +0300

    TemplatesCache is made threadsafe.

--- src/com/epam/xsl/command/util/TemplatesCache.java
+++ src/com/epam/xsl/command/util/TemplatesCache.java
@@ -1,19 +1,23 @@
 package com.epam.xsl.command.util;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import javax.xml.transform.Templates;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamSource;
 
-public class TemplatesCache {
-	private static Map<String, MapEntry> cache = new ConcurrentHashMap<String, MapEntry>();
+public final class TemplatesCache {
+	private static Map<String, TemplatesWrapper> cache = new HashMap<String, TemplatesWrapper>();
 
 	private static final TransformerFactory transformerFactory = TransformerFactory
 			.newInstance();
+
+	private static final Lock lock = new ReentrantLock();
 
 	private TemplatesCache() {
 	}
@@ -21,36 +25,47 @@ public class TemplatesCache {
 	public static Templates getTemplates(String xsltPath)
 			throws TransformerConfigurationException {
 		File xsltFile = new File(xsltPath);
+		TemplatesWrapper templ = cache.get(xsltPath);
 		long lastModified = xsltFile.lastModified();
-		MapEntry entry = cache.get(xsltPath);
 
-		if (entry != null) {
-			if (lastModified > entry.lastModified) {
-				entry = null;
+		if (templ != null) {
+			// check if XSLT file was changed
+			if (lastModified > templ.lastModified) {
+				templ = null;
 			}
 		}
 
-		if (entry == null) {
-			entry = createEntry(lastModified, xsltPath);
+		if (templ == null) {
+			templ = newEntryInCache(lastModified, xsltPath);
 		}
-
-		return entry.templates;
+		return templ.templates;
 	}
 
-	private static MapEntry createEntry(long lastModified,
-			String entryKey) throws TransformerConfigurationException {
-		Templates templates = transformerFactory.newTemplates(new StreamSource(
-				entryKey));
-		MapEntry entry = new MapEntry(templates, lastModified);
-		cache.put(entryKey, entry);
-		return entry;
+	private static TemplatesWrapper newEntryInCache(long lastModified, String key)
+			throws TransformerConfigurationException {
+		lock.lock();
+		try {
+			TemplatesWrapper templWrapper = cache.get(key);
+			// check if other thread updated entry in a cache
+			if (lastModified == templWrapper.lastModified) {
+				return templWrapper;
+			}
+			// if didn't, put new entry in a cache
+			Templates templates = transformerFactory
+					.newTemplates(new StreamSource(key));
+			templWrapper = new TemplatesWrapper(templates, lastModified);
+			cache.put(key, templWrapper);
+			return templWrapper;
+		} finally {
+			lock.unlock();
+		}
 	}
 
-	private static class MapEntry {
+	private static class TemplatesWrapper {
 		Templates templates;
 		long lastModified;
 
-		MapEntry(Templates templates, long lastModified) {
+		TemplatesWrapper(Templates templates, long lastModified) {
 			this.templates = templates;
 			this.lastModified = lastModified;
 		}
